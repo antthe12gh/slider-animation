8ce286da8ba4a7245ffc49bcb210372e71857a0a
antthe12gh <dinhthe2000gh@gmail.com>
2022-09-09 15:20:42 +0700

    upload code

---
+++ index.js
@@ -0,0 +1,34 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const alls = $$('.container');
+
+const down = $('.ti-arrow-down');
+const up = $('.ti-arrow-up');
+
+
+let index = 1;
+
+down.onclick = function(){
+
+    $('.active').classList.remove('active');
+    alls[index].classList.add('active')
+
+
+    index++;
+    if (index == alls.length){
+        index = 0;
+    }
+    
+}
+
+let dem = 3;
+up.onclick = function(){
+    $('.active').classList.remove('active');
+    alls[dem].classList.add('active')
+    dem--;
+    if (dem < 0){
+        dem = alls.length - 1;
+    }
+    
+}
